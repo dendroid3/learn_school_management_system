c9fdecd31b11c8571f6e1e9e8bcbecb77c11dbb4
Denis Wanjohi <kazibin66@gmail.com>
2024-05-30 16:46:56 +0300

    add functionality to add student

--- addstudent.php
+++ addstudent.php
@@ -11,6 +11,49 @@
         header("location:login.php");
     }
 
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $db = "school_management";
+
+    $data = mysqli_connect($host, $user, $password, $db);
+
+    if(isset($_POST['Add_student'])){
+        $username = $_POST['name'];
+        $phone = $_POST['phone'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $user_type = 'student';
+
+        $check_username = "SELECT * FROM users WHERE username = '$username'";
+        $check_result = mysqli_query($data, $check_username);
+        $row_count = mysqli_num_rows($check_result);
+
+        if($row_count === 1){
+            echo 
+            "<script type='text/javascript'>
+                alert('Username already taken, please select another username');
+            </script>";
+        } else {
+            $sql = "INSERT INTO users(username, phone, email, password, user_type) VALUES ('$username', '$phone', '$email', '$password', '$user_type')";
+
+            $result = mysqli_query($data, $sql);
+    
+            if($result){
+                echo 
+                "<script type='text/javascript'>
+                    alert('Student Added Successfully')
+                </script>";
+            } else {
+                echo 
+                "<script type='text/javascript'>
+                    alert('Student Could not be added')
+                </script>";
+            }
+        }
+
+    }
+
 ?>
 
 <!DOCTYPE html>
@@ -20,9 +63,48 @@
     ?>
         <center>
             <div class="content">
-                I am add student
+                <h1>Add Student</h1>
+
+                <div class="div_deg">
+                    <form action="#" method="POST">
+                        <div>
+                            <label>Username</label>
+                            <input type="text" name="name">
+                        </div>
+                        <div>
+                            <label>Phone</label>
+                            <input type="text" name="phone">
+                        </div>
+                        <div>
+                            <label>email</label>
+                            <input type="text" name="email">
+                        </div>
+                        <div>
+                            <label>Password</label>
+                            <input type="password" name="password">
+                        </div>
+                        <div>
+                            <input type="submit" class="btn btn-primary" name="Add_student" value="Add Student">
+                        </div>
+                    </form>
+                </div>
             </div>
         </center>
+        <style>
+            label{
+                text-align: right;
+                display: inline-block;
+                width: 100px;
+                padding-top: 10px;
+                padding-bottom: 10px;
+            }
+
+            .div_deg{
+                background-color: skyblue;
+                width: 400px;
+                padding: 70px 0;
+            }
+        </style>
     </body>
 </html>
 
