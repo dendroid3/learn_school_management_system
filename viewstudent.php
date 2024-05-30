aa7b324fc684ad6f2b03fb92e0ccdc351f524fef
Denis Wanjohi <kazibin66@gmail.com>
2024-05-30 17:37:12 +0300

    add functionality to view students

--- viewstudent.php
+++ viewstudent.php
@@ -11,6 +11,17 @@
         header("location:login.php");
     }
 
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $db = "school_management";
+
+    $data = mysqli_connect($host, $user, $password, $db);
+
+    $sql = "SELECT * FROM users WHERE user_type = 'student'";
+
+    $result = mysqli_query($data, $sql);
+
 ?>
 
 <!DOCTYPE html>
@@ -20,7 +31,40 @@
     ?>
         <center>
             <div class="content">
-                I am view student
+                <h1>Student Data</h1>
+
+                <table class="table" style="width: 75%;">
+                    <thead>
+                        <th>Username</th>
+                        <th>Phone Number</th>
+                        <th>Email</th>
+                    </thead>
+                    <tbody>
+                        <?php
+                            while($info = $result->fetch_assoc()){
+                        ?>
+                            <tr style="padding: 20px;">
+                                <td>
+                                    <?php
+                                        echo "{$info['username']}";
+                                    ?>
+                                </td>
+                                <td>
+                                    <?php
+                                        echo "{$info['phone']}";
+                                    ?>
+                                </td>
+                                <td>
+                                    <?php
+                                        echo "{$info['email']}";
+                                    ?>
+                                </td>
+                            </tr>
+                        <?php
+                            }
+                        ?>
+                    </tbody>
+                </table>
             </div>
         </center>
     </body>
